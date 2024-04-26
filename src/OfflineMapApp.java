b64d75dd23903fd258e71691f9a737490f567805
oreste-abizera <oresteabizera11@gmail.com>
2024-04-26 21:28:28 +0200

    added initial files

---
+++ src/OfflineMapApp.java
@@ -0,0 +1,32 @@
+import de.fhpotsdam.unfolding.UnfoldingMap;
+import de.fhpotsdam.unfolding.providers.MBTilesMapProvider;
+import de.fhpotsdam.unfolding.utils.MapUtils;
+import processing.core.PApplet;
+
+/**
+ * This example uses a local MBTiles file. Thus, it does not need an Internet connection to load tiles.
+ */
+public class OfflineMapApp extends PApplet {
+
+	public static String mbTilesString = "blankLight-1-3.mbtiles";
+
+	UnfoldingMap map;
+
+	public void setup() {
+		size(800, 600);
+
+		map = new UnfoldingMap(this, new MBTilesMapProvider(mbTilesString));
+		MapUtils.createDefaultEventDispatcher(this, map);
+		map.setZoomRange(1, 3);
+	}
+
+	public void draw() {
+		background(0);
+		map.draw();
+	}
+
+	public static void main(String[] args) {
+		PApplet.main(new String[] { OfflineMapApp.class.getName() });
+	}
+
+}
